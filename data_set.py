292c579a8f64e3ab2f5437a7a983d29eec22e49f
maxlv7 <admin@maxlv.org>
2020-07-23 13:56:50 +0800

    解决hdf5多进程加载Bug

--- data_set.py
+++ data_set.py
@@ -6,14 +6,16 @@ import h5py
 import numpy as np
 import torch
 import torch.utils.data as dt
+from torch.utils.data.dataloader import DataLoader
 
 
 class ISONetData(dt.Dataset):
 
     def __init__(self, data_path=None, train=True, gray_mode=False, shuffle=False) -> None:
+        print("init...")
         if data_path is not None:
-            data_path = Path(data_path)
-            if not data_path.exists():
+            self.data_path = Path(data_path)
+            if not self.data_path.exists():
                 print("data path not exist!")
                 exit(0)
 
@@ -31,24 +33,43 @@ class ISONetData(dt.Dataset):
             self.val_h5_label = 'val_rgb_label.h5'
 
         if self.train:
-            self.h5 = h5py.File(data_path.joinpath(self.train_h5), 'r')
-            self.h5_label = h5py.File(data_path.joinpath(self.train_h5_label), 'r')
+            self.h5 = h5py.File(self.data_path.joinpath(self.train_h5), 'r')
+            self.h5_label = h5py.File(self.data_path.joinpath(self.train_h5_label), 'r')
         else:
-            self.h5 = h5py.File(data_path.joinpath(self.val_h5), 'r')
-            self.h5_label = h5py.File(data_path.joinpath(self.val_h5_label), 'r')
+            self.h5 = h5py.File(self.data_path.joinpath(self.val_h5), 'r')
+            self.h5_label = h5py.File(self.data_path.joinpath(self.val_h5_label), 'r')
 
         self.keys = list(self.h5.keys())
         if shuffle:
             random.shuffle(self.keys)
+        self.len = self.h5.__len__()
+        self.h5.close()
+        self.h5_label.close()
 
     def __len__(self) -> int:
-        return len(self.keys)
+        return self.len
 
     def __getitem__(self, index: int):
-        data = torch.from_numpy(np.array(self.h5.get(str(index)))).to(dtype=torch.float32)
-        label = torch.from_numpy(np.array(self.h5_label.get(str(index)))).to(dtype=torch.float32)
+        if self.train:
+            h5 = h5py.File(self.data_path.joinpath(self.train_h5), 'r')
+            h5_label = h5py.File(self.data_path.joinpath(self.train_h5_label), 'r')
+        else:
+            h5 = h5py.File(self.data_path.joinpath(self.val_h5), 'r')
+            h5_label = h5py.File(self.data_path.joinpath(self.val_h5_label), 'r')
+        data = torch.from_numpy(np.array(h5[str(index)])).to(dtype=torch.float32)
+        # print(f">>> index:[{index}],label:{np.array(h5_label[str(index)])}")
+        label = torch.from_numpy(np.array(h5_label[str(index)])).to(dtype=torch.float32)
+        h5.close()
+        h5_label.close()
         return data, label
+
+    def __del__(self):
+        print("del.........")
 
 
 if __name__ == '__main__':
-    data = ISONetData(data_path="ttt", train=False)
+
+    dataset = ISONetData(data_path="data_64_32")
+    data_loader = DataLoader(dataset=dataset, batch_size=128, shuffle=True, num_workers=8, pin_memory=True)
+    for i, (data, label) in enumerate(data_loader, 0):
+        print(i)
