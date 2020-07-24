833f946005aa64548179c03c0c07728884cb9039
maxlv7 <admin@maxlv.org>
2020-07-24 22:07:05 +0800

    delete unuseful file

--- test_code.py
+++
@@ -1,9 +0,0 @@
-import time
-
-s = time.process_time()
-
-for x in range(1000000):
-    pass
-e = time.process_time()
-
-print(e-s)
--- test_summary.py
+++
@@ -1,7 +0,0 @@
-from torch.utils.tensorboard import SummaryWriter
-
-
-
-writer = SummaryWriter()
-
-writer.add_scalar()
